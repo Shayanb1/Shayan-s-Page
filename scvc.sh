aa519fedf35e57223594e82f52473a9b2ba66b43
shayanb1 <shayanb1245@gmail.com>
2024-12-09 12:23:35 -0800

    Car

---
+++ scvc.sh
@@ -0,0 +1,39 @@
+%%script bash
+
+# This example has an error in VSCode; it runs best on Jupyter
+cd /tmp
+
+file="sample.md"
+if [ -f "$file" ]; then
+    rm $file
+fi
+
+# Create a markdown file using tee and here document (<<EOF)
+tee -a $file >/dev/null <<EOF
+# Show Generated Markdown
+This introductory paragraph and this line and the title above are generated using tee with the standard input (<<) redirection operator.
+- This bulleted element is still part of the tee body.
+EOF
+
+# Append additional lines to the markdown file using echo and redirection (>>)
+echo "- This bulleted element and lines below are generated using echo with standard output (>>) redirection operator." >> $file
+echo "- The list definition, as is, is using space to separate lines. Thus the use of commas and hyphens in output." >> $file
+
+# Define an array of actions and their descriptions
+actions=("ls,list-directory" "cd,change-directory" "pwd,present-working-directory" "if-then-fi,test-condition" "env,bash-environment-variables" "cat,view-file-contents" "tee,write-to-output" "echo,display-content-of-string" "echo_text_>\$file,write-content-to-file" "echo_text_>>\$file,append-content-to-file")
+
+# Loop through the actions array and append each action to the markdown file
+for action in ${actions[@]}; do
+  action=${action//-/ }  # Convert dash to space
+  action=${action//,/: } # Convert comma to colon
+  action=${action//_text_/ \"sample text\" } # Convert _text_ to "sample text", note escape character \ to avoid "" having meaning
+  echo "    - ${action//-/ }" >> $file  # Append action to file
+done
+
+echo ""
+echo "File listing and status"
+ls -l $file # List file details
+wc $file   # Show word count
+mdless $file  # Render markdown from terminal (requires mdless installation)
+
+rm $file  # Clean up temporary file
